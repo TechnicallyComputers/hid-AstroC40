7bd66ce0cc9cfe2fa49a5ec8e58f8b29effb7551
Alex Vanderveen <alex@technicallycomputers.ca>
2026-03-18 23:06:33 -0400

    Add HID device IDs for standalone DKMS build

---
+++ hid-ids.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * HID device IDs for standalone DKMS build.
+ * When building against kernel source, use #include <linux/hid-ids.h> instead
+ * if these IDs are present in the kernel.
+ */
+#ifndef _HID_IDS_H
+#define _HID_IDS_H
+
+#define USB_VENDOR_ID_ASTRO		0x9886
+#define USB_DEVICE_ID_ASTRO_C40		0x0025
+#define USB_DEVICE_ID_ASTRO_C40_2	0x0024
+
+#endif
